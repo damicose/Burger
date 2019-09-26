fa167a13ba43582dbb85a853b9582e8a8bab248b
Sonny D <damicose14@gmail.com>
2019-09-26 18:10:53 -0500

    server connection successful

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
-  database: "movie_planner_db"
+  password: "root",
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
@@ -37,7 +37,7 @@ connection.connect(function(err) {
 
 // Use Handlebars to render the main index.html page with the movies in it.
 app.get("/", function(req, res) {
-  connection.query("SELECT * FROM movies;", function(err, data) {
+  connection.query("SELECT * FROM burgers;", function(err, data) {
     if (err) {
       return res.status(500).end();
     }
